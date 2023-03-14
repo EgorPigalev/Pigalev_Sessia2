ac10ec39c9f44966cfdee4cce2a5ecb7f87f0ac6
Егор Пигалёв <1pigaleve@mail.ru>
2023-03-14 18:03:11 +0300

    Добавьте файлы проекта.

---
+++ SF2022User{NN}Lib/Calculations.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF2022User_NN_Lib
+{
+    public class Calculations
+    {
+        /// <summary>
+        /// Метод, который возвращает список свободных временных интервалов в графике сотрудника
+        /// </summary>
+        /// <param name="startTimes"></param>
+        /// <param name="durations"></param>
+        /// <param name="beginWorkingTime"></param>
+        /// <param name="endWorkingTime"></param>
+        /// <param name="consultationTime"></param>
+        /// <returns></returns>
+        public static string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consultationTime)
+        {
+            string[] listFreePeriods = new string[0];
+            TimeSpan time = beginWorkingTime;
+            TimeSpan gap = new TimeSpan(0, consultationTime, 0);
+            while(time < endWorkingTime)
+            {
+                TimeSpan timeSpan = getProverkaPeriod(time, time.Add(gap), startTimes, durations);
+                if (timeSpan != new TimeSpan())
+                {
+                    TimeSpan timeSpanEnd = timeSpan.Add(-gap);
+                    if (timeSpanEnd > beginWorkingTime)
+                    {
+                        Array.Resize(ref listFreePeriods, listFreePeriods.Length + 1);
+                        listFreePeriods[listFreePeriods.Length - 1] = "" + timeSpan.Add(-gap).ToString() + " - " + timeSpan.ToString();
+                    }
+                }
+                else
+                {
+                    Array.Resize(ref listFreePeriods, listFreePeriods.Length + 1);
+                    listFreePeriods[listFreePeriods.Length - 1] = "" + time.ToString() + " - " + time.Add(gap).ToString();
+                }
+                time = time.Add(gap);
+            }
+            return listFreePeriods;
+        }
+
+        public static TimeSpan getProverkaPeriod(TimeSpan timeStart, TimeSpan timeEnd, TimeSpan[] startTimes, int[] durations)
+        {
+            for(int i = 0; i < startTimes.Length; i++)
+            {
+                TimeSpan startSession = startTimes[i];
+                TimeSpan timeAdd = new TimeSpan(0, durations[i], 0);
+                TimeSpan endSession = startTimes[i].Add(timeAdd);
+                if(timeStart < startSession.Add(new TimeSpan(0, 0, 1)))
+                {
+                    if(timeEnd >= endSession.Add(new TimeSpan(0, 0, 1)))
+                    {
+                        return startSession;
+                    }
+                }
+            }
+            return new TimeSpan();
+        }
+    }
+}
---
+++ UnitTest/UnitTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SF2022User_NN_Lib;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void AvailablePeriods_Correctly()
+        {
+            string[] expected = new string[0];
+            TimeSpan[] startTimes = new TimeSpan[2];
+            int[] durations = new int[2];
+            startTimes[0] = new TimeSpan(2,15,0);
+            durations[0] = 15;
+            startTimes[1] = new TimeSpan(3, 0, 0);
+            durations[1] = 30;
+            TimeSpan beginWorkingTime = new TimeSpan(1, 0, 0);
+            TimeSpan endWorkingTime = new TimeSpan(4, 0, 0);
+            int consultationTime = 30;
+            string[] actual = Calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
